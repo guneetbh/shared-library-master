992e5948ebd680b46d48d37de126ec8cd479aeed
guneetbh <guneet.bhatia@ibm.com>
2021-05-31 17:56:55 +0530

    Updated code

--- vars/Shared_lib_file_operations.groovy
+++ vars/Shared_lib_file_operations.groovy
@@ -141,7 +141,8 @@ for (File file in filesList) {
 
 count++
 
-File remoteFile = new File(file.replace(from, to))
+println "remoteFile Path : ${file.replace(from, to)}"
+//File remoteFile = new File(file.replace(from, to))
 println("Files in remoteFile :  ${remoteFile}")
 String res = takeCareOnOneFile(count, filesList.size(), file, remoteFile, verbose, silent, skipNewer)
 
