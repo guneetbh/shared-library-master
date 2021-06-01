61a1ce2d4a1dc521db79fa179032d060d42aaf85
guneetbh <guneet.bhatia@ibm.com>
2021-06-01 17:36:46 +0530

    Updated code

--- vars/Shared_lib_file_operations.groovy
+++ vars/Shared_lib_file_operations.groovy
@@ -206,21 +206,8 @@ FilePath targetDir = createFilePath(target);
 if(!targetDir.exists()){
 	targetDir.mkdirs()
 }
-FilePath ws = build.getWorkspace();
-      if (ws == null) {
-        throw new IllegalStateException("The workspace should be created when setUp method is called");
-      }
-      if (!ALLOW_FOLDER_TRAVERSAL_OUTSIDE_WORKSPACE && !ws.isDescendant(location)) {
-        listener.error("Rejecting file path escaping base directory with relative path: " + location);
-        // force the build to fail
-        return null;
-      }
- 
-       
-				 FilePath locationFilePath = ws.child(sourceFile);
-      locationFilePath.getParent().mkdirs();
-      locationFilePath.copyFrom(sourceFile);
-      locationFilePath.copyTo(new FilePath(targetDir));//getLocationUnderBuild(build)));
+
+sourceFile.copyTo(targetDir);
            // Files.copy(sourceFile, targetFile);
 			//Files.copy(p1,p2, StandardCopyOption.REPLACE_EXISTING)
         
