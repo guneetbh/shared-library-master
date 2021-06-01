418e6b1716f7f4814b6527c62f22b6d590bbafcc
guneetbh <guneet.bhatia@ibm.com>
2021-06-01 18:37:52 +0530

    Updated code

--- vars/Shared_lib_file_operations.groovy
+++ vars/Shared_lib_file_operations.groovy
@@ -208,6 +208,7 @@ if(!targetDir.exists()){
 }
 println("Moving all children to target dir")
 targetDir.chmod(0777)
+targetDir.replaceAll(" ", "\\ ")
 sourceFile.copyTo(targetDir);
            // Files.copy(sourceFile, targetFile);
 			//Files.copy(p1,p2, StandardCopyOption.REPLACE_EXISTING)
